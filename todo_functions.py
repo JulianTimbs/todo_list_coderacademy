1909d6cf674cf562057c890fd13e2784f2fc2fe8
Julian <timbsjulian@gmail.com>
2023-12-09 10:52:25 +1000

    Added remove function

--- todo_functions.py
+++ todo_functions.py
@@ -9,9 +9,29 @@ def add_todo(file_name):
 
 def remove_todo(file_name):
     print("Remove todo")
+    todo_name = input("Enter todo to be removed: ")
+    todo_lists = []
+    with open(file_name, "r") as f:
+        reader = csv.reader(f)
+        for row in f:
+            if (todo_name != row[0]):
+                todo_lists.append(row)
+    print(todo_lists)
+    with open(file_name, "w") as f:
+        writer = csv.writer(f)
+        writer.writerows(todo_lists)
 
 def mark_todo(file_name):
     print("Mark todo")
 
 def view_todo(file_name):
     print("View todo")
+    with open(file_name, "r") as f:
+        reader = csv.reader(f)
+        reader.__next__()
+        for row in reader:
+            # row = ["Todo 1", "False"]
+            if (row[1] == "True"):
+                print(f"Todo {row[0]} is complete")
+            else:
+                print(f"Todo {row[0]} is not complete")
